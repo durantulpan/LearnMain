cfdf54b520d5edb1769e0eb4630ae642dab769aa
durantulpan <jackmilos7@gmail.com>
2020-09-17 20:07:20 +0300

    First program

---
+++ src/LearnMain.java
@@ -0,0 +1,8 @@
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava"+11);
+        System.out.println();
+    }
+}
